766f934fc6038fec4cff2836536413df47a22493
sQuishy2505 <66022725@up.ac.th>
2024-11-22 17:24:40 +0700

    Navbar

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,3 +1,4 @@
+
 export default function WebsiteLayout({
     children,
 }: {
@@ -5,8 +6,16 @@ export default function WebsiteLayout({
 }) {
     return (
       <>
-      <div className="w-full h-[100] bg-[#ffffff] shadow-xl">
-        <div className="container"></div>
+      <div className="w-full h-[90] bg-[transparent] ">
+        <div className="Home text-black py-[30px] w-auto pl-[600] pr-[10px] text-[12px] px-[10px] flex justify-start ... ">
+          <div className = "flex-[0.2] mx-[10] py-[10]  text-center hover:bg-[#0c6478] hover:text-white rounded-[40px] mx-[10px]">HOME</div>
+          <div className = "flex-[0.2] py-[10] text-center hover:bg-[#0c6478] hover:text-white rounded-[40px] mx-[10px]">ABOUT</div>
+          <div className = "flex-[0.2] py-[10] text-center hover:bg-[#0c6478] hover:text-white rounded-[40px] mx-[10px]">BMI</div>
+          <div className = "flex-[0.2] py-[10] text-center hover:bg-[#0c6478] hover:text-white rounded-[40px] mx-[10px] ">SETTING</div>
+          <div className = "flex-[0.2] py-[10] bg-[#213A58] text-white  text-center rounded-[40px] mx-[10px] hover:text-[#213A58] hover:bg-[#ffffff] hover:rounded-[40px] hover:border-solid border-2 border-[#213A58] ...">PROFILE</div>
+
+        </div>
+        
       </div>
       <div className="">{children}</div>
       </>
--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <>
-      <body className="bg-[#fff]">
+      <body className="bg-[#ffffff]">
       </body>
       <div>
         <h1>หน้าหลัก111หหห</h1>
