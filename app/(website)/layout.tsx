f29a786e14dbd6adac3837b7ba39bd818cc71a57
sQuishy2505 <66022725@up.ac.th>
2024-11-23 01:02:00 +0700

    register

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -1,4 +1,13 @@
+'use client';
 
+import { useRouter } from 'next/router';
+
+const ProfileButton = () => {
+    const router = useRouter();
+
+    const handleNavigate = () => {
+        router.push('/login'); 
+    };}
 export default function WebsiteLayout({
     children,
 }: {
@@ -9,12 +18,12 @@ export default function WebsiteLayout({
       <div className="w-full h-[auto] bg-[transparent] ">
         <div className=" text-black py-[30px] w-auto pr-[10px] text-[12px] px-[10px] flex justify-start ... ">
         <div className = "py-[10] text-left pr-[500] ml-[100] text-[16px] font-mono">FITLIFE_HUB</div>
-          <div className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">HOME</div>
-          <div className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">ABOUT</div>
-          <div className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">BMI</div>
-          <div className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px] ">SETTING</div>
-          <div className = "flex-[0.2] py-[10] mt-[5] bg-[#213A58] text-white  text-center rounded-[40px] mx-[10px] hover:text-[#213A58] hover:bg-[#ffffff] hover:rounded-[40px] hover:border-solid border-2 border-[#213A58] ...">PROFILE</div>
-
+          <button className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">HOME</button>
+          <button className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">ABOUT</button>
+          <button className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px]">BMI</button>
+          <button className = "flex-[0.2] py-[10] mt-[5] text-center hover:bg-[#213A58] hover:text-white rounded-[40px] mx-[10px] ">SETTING</button>
+          <button className = "flex-[0.2] py-[10] mt-[5] bg-[#213A58] text-white  text-center rounded-[40px] mx-[10px] hover:text-[#213A58] hover:bg-[#ffffff] hover:rounded-[40px] hover:border-solid border-2 border-[#213A58] ...">
+          PROFILE</button>
         </div>
         
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,34 +1,37 @@
-import Link from 'next/link';
-export default function LoginPage () {
+export default function RegisterPage () {
     return (
       <>
       <div className="flex flex-col items-center justify-center min-h-screen bg-[#e2e2e2]">
   <div className="flex flex-col md:flex-row bg-white rounded-lg shadow-lg overflow-hidden">
-    <div className="flex-1 flex items-center justify-center p-8 bg-[#213A58] text-white">
+    <div className="flex-1 flex items-center justify-center p-8 bg-[#213A58] text-white w-[500px]">
       <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4">LOGIN</h1>
-        <img aria-hidden="true" alt="illustration" src="/sp2.png" />
+        <h1 className="text-2xl font-bold mb-4">REGISTER</h1>
+        <img aria-hidden="true" alt="illustration" src="/sp.png" />
       </div>
     </div>
     <div className="flex-1 p-8">
-      <h2 className="text-xl font-semibold mb-6 text-black">Login</h2>
+      <h2 className="text-xl font-semibold mb-6 text-black">REGISTER</h2>
       <form>
         <div className="mb-4">
           <label htmlFor="username" className="block text-sm font-medium text-zinc-700">Username</label>
           <input type="text" id="username" className="mt-1 block w-full border border-border rounded-md shadow-sm p-2" placeholder="Enter your username" required />
         </div>
         <div className="mb-4">
+          <label htmlFor="username" className="block text-sm font-medium text-zinc-700">Email</label>
+          <input type="email" id="Email" className="mt-1 block w-full border border-border rounded-md shadow-sm p-2" placeholder="Enter your Email" required />
+        </div>
+        <div className="mb-4">
           <label htmlFor="password" className="block text-sm font-medium text-zinc-700">Password</label>
           <input type="password" id="password" className="mt-1 block w-full border border-border rounded-md shadow-sm p-2" placeholder="Enter your password" required />
         </div>
+        
         <div className="flex items-center mb-4">
           <input type="checkbox" id="remember" className="mr-2" />
-          <label htmlFor="remember" className="text-sm text-black">Remember me</label>
+          <label htmlFor="remember" className="text-sm text-black">ACCEPT</label>
         </div>
-        <button type="submit" className="w-full bg-primary text-primary-foreground bg-black text-white hover:bg-transparent hover:text-black border-solid border-2 border-black  p-2 rounded-md">Login</button>
+        <button type="submit" className="w-full bg-primary text-primary-foreground bg-black text-white hover:bg-transparent hover:text-black border-solid border-2 border-black  p-2 rounded-md">REGISTER</button>
       </form>
-      <p className="mt-4 text-sm text-muted-foreground text-black">Don't have an account yet? <a href="#" className="text-primary">Create an account</a></p>
-      <p className="mt-2 text-sm text-muted-foreground text-[#213A58]"><a href="#" className="text-primary">Forgot Password?</a></p>
+    
     </div>
   </div>
 </div>        
