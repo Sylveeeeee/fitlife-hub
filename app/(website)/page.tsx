ced7b0e0fd10491957659e234b81afa7c471d7a7
sQuishy2505 <66022725@up.ac.th>
2024-11-24 01:12:08 +0700

    feee
    fesfsf

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       </body>
       <div className= "text-black font-mono flex flex-col mx-[200px]  ">
        <div className="py-[15] text-[24px] font-semibold">Your Dashboard</div>
-       <div className="bg-white flex justify-between pl-[20] ">
+       <div className="bg-white flex justify-between pl-[20] rounded-[4npm install react-icons --save] ">
         <div className="py-[10]  font-semibold text-[18px]">Quick Add to Diary</div>
         <div className="flex items-center justify-between w-[60%] mr-[100] ml-[100] h-[50]">
           <button className="flex items-center justify-center hover:border-b-4 hover:border-black border-b-4 border-transparent pb-[9] pt-[13]"><div className="mr-[6]"><VscDiffAdded /></div>FOOD</button>
@@ -20,5 +20,7 @@ export default function Home() {
         </div>        
       </>
     );
-  }
+  } 
+
+  
   
