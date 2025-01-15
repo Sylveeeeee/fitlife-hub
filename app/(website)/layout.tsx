126aacc6559054b54628b19f37b2d207d36af31d
sQuishy2505 <66022725@up.ac.th>
2025-01-15 20:43:45 +0700

    lwok

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -13,13 +13,20 @@ export default function WebsiteLayout({
         <div className="">
           <div className = "  ml-[100]  text-[16px] font-mono text-[#000]">FITLIFE_HUB ฟหกฟหก </div>
         </div>
-        <div className="font-mono text-[#000] h-[100] items-center mr-[30] mt-[50px] flex">
-            <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">HOME</button>
-            <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">ABOUT</button>
-            <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">BMI</button>
-            <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">SETTING</button>
-            <button className = "h-[70] w-[70]  text-center rounded-full mx-[10px] flex justify-center items-center">
-            <div className="text-[30px]"><FiUser /></div></button>
+        <div className="font-mono text-[#000] h-[100] items-center mr-[30] flex">
+        <Link href = "/">
+          <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">DASHBOARD</button>
+        </Link>
+        <Link href = "/diary">
+          <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">DIARY</button>
+        </Link>       
+          <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">ABOUT</button>
+           <button className = "py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent">BMI</button>
+        <Link href = "/login">
+         <button className = "h-[70] w-[70]  text-center rounded-full mx-[10px] flex justify-center items-center">
+            <div className="text-[30px]"><FiUser /></div>
+          </button>
+        </Link>
         </div>
        
         </div>
