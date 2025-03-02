b265f649b5ea3efe1f95faad6aa328f89a335683
jirawat <66022477@up.ac.th>
2025-03-02 16:09:25 +0700

    🎟️Update stye navbar(3)

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,6 +2,8 @@
 import { FiUser } from "react-icons/fi";
 import Link from "next/link";
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 interface JwtPayload {
   userId: bigint;
@@ -16,8 +18,11 @@ export default function WebsiteLayout({
 }) {
   const [user, setUser] = useState<JwtPayload | null>(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true); // สร้าง state สำหรับการโหลด
+  const pathname = usePathname(); // ดึง path ปัจจุบัน เช่น "/diary"
 
   useEffect(() => {
+  
     const fetchUserData = async () => {
       try {
         const response = await fetch('/api/auth/protected', {
@@ -29,14 +34,13 @@ export default function WebsiteLayout({
           const data = await response.json();
           if (data && data.userId && data.role) {
             const role = typeof data.role === 'object' ? data.role.name : data.role;
-            
+
             setUser({
               userId: data.userId,
               role: data.role,
               email: data.email,
             });
             if (role.toLowerCase() === 'admin') {
-
             }
           } else {
             console.error("Invalid user data from server");
@@ -54,9 +58,14 @@ export default function WebsiteLayout({
         setUser(null);
       }
     };
+    
 
     fetchUserData();
-  }, []);
+    setTimeout(() => {
+      setLoading(false); // เปลี่ยนสถานะการโหลดเมื่อโหลดเสร็จ
+    }, 2000);
+    
+  }, [isPopupOpen]);
 
   async function handleLogout() {
     try {
@@ -95,6 +104,7 @@ export default function WebsiteLayout({
     window.location.href = "/login";
   };
 
+
   useEffect(() => {
     if (isPopupOpen) {
       document.body.classList.add("overflow-hidden");
@@ -104,38 +114,74 @@ export default function WebsiteLayout({
     return () => document.body.classList.remove("overflow-hidden");
   }, [isPopupOpen]);
 
-  
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen ">
+        {/* แสดง spinner หรือข้อความระหว่างที่โหลด */}
+        <div className="loader text-black">Loading...</div>
+      </div>
+    );
+  }
+  
   return (
     <>
-      <div className="w-full h-[100] flex items-center justify-between"
-        onClick={handleOutsideClick}>
-        <div>
-          <div className="ml-[100] text-[16px] font-mono text-[#000]">FITLIFE_HUB</div>
+      <div
+        className="w-full h-[100px] flex items-center justify-between px-10"
+        onClick={handleOutsideClick}
+      >
+        <Link href="/">
+        <div className="flex items-center  ">
+          <Image
+            src="/logo.png" // ใส่ Path ของรูปภาพของคุณ
+            alt="FitLifeHub Logo"
+            width={130} // ปรับขนาดของรูปภาพ
+            height={100} // ปรับขนาดของรูปภาพ
+            className="mr-2 rounded-[20px]"
+          />        
         </div>
-        <div className="font-mono text-[#000] h-[100] items-center mr-[30] flex">
-          <Link href="/">
-            <button className="py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold">
-              DASHBOARD
-            </button>
-          </Link>
-          <Link href="/diary">
-            <button className="py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold">
-              DIARY
-            </button>
-          </Link>
-          <Link href="/posts">
-            <button className="py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold">
-              POST
-            </button>
-          </Link>
-          <Link href="/BMI"><button className="py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold">
-            BMI
-          </button></Link>
+        </Link>
+        <div className="font-mono text-[#000] h-[100px] items-center flex">
+      <Link href="/">
+        <button
+          className={`py-[10px] px-[50px] text-center mx-[10px] border-b-4 hover:bg-[#0000000a] hover:font-bold hover:text-[#213A58] ${
+            pathname === "/" ? "border-[#213A58] text-[#213A58] font-bold" : "border-transparent " 
+          }`}
+        >
+          DASHBOARD
+        </button>
+      </Link>
+      <Link href="/diary">
+        <button
+          className={`py-[10px] px-[50px] text-center mx-[10px] border-b-4 hover:bg-[#0000000a] hover:font-bold hover:text-[#213A58] ${
+            pathname === "/diary" ? "border-[#213A58] text-[#213A58] font-bold" : "border-transparent"
+          }`}
+        >
+          DIARY
+        </button>
+      </Link>
+      <Link href="/posts">
+        <button
+          className={`py-[10px] px-[50px] text-center mx-[10px] border-b-4 hover:bg-[#0000000a] hover:font-bold hover:text-[#213A58] ${
+            pathname === "/posts" ? "border-[#213A58] text-[#213A58] font-bold" : "border-transparent"
+          }`}
+        >
+          POST
+        </button>
+      </Link>
+      <Link href="/BMI">
+        <button
+          className={`py-[10px] px-[50px] text-center mx-[10px] border-b-4 hover:bg-[#0000000a]  hover:font-bold hover:text-[#213A58] ${
+            pathname === "/BMI" ? "border-[#213A58] text-[#213A58] font-bold  " : "border-transparent  "
+          }`}
+        >
+          CALCULATOR
+        </button>
+      </Link>
           {user ? (
             <div className="flex items-center relative ">
               <button
-                className="py-[10] px-[50] text-center hover:text-[#213A58] hover:border-b-4 hover:border-[#213A58] mx-[10px] hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold"
+                className="py-[10px] px-[50px] text-center hover:text-[#213A58]  hover:bg-[#0000000a] border-b-4 border-transparent hover:font-bold"
                 onClick={handlePopupToggle}
               >
                 Account
@@ -143,7 +189,7 @@ export default function WebsiteLayout({
             </div>
           ) : (
             <button
-              className="h-[40px] w-[40px] text-center rounded-full mx-[10px] flex justify-center items-center"
+              className="h-[40px] w-[40px] text-center rounded-full  flex justify-center items-center"
               onClick={handleLoginRedirect}
             >
               <div className="text-[30px]">
@@ -153,14 +199,16 @@ export default function WebsiteLayout({
           )}
         </div>
       </div>
+
+      {/* Popup */}
       {isPopupOpen && user && (
         <div
           className="fixed w-full h-full top-16 flex justify-center items-center font-mono text-[#000] z-[9999]"
-          onClick={handleOutsideClick}  // ทำการปิด modal เมื่อคลิกนอก
+          onClick={handleOutsideClick}
         >
           <div
             className="bg-white p-6 w-[400px] shadow-lg fixed top-20 right-10"
-            onClick={(e) => e.stopPropagation()}  // หยุดการ propagate event
+            onClick={(e) => e.stopPropagation()}
           >
             <h2 className="text-lg font-bold mb-4">Current Account</h2>
             <div>
@@ -170,8 +218,7 @@ export default function WebsiteLayout({
                   Targets+Profile
                 </button>
               </Link>
-              {/* แสดงปุ่ม Admin Dashboard หาก role เป็น admin */}
-              {user.role.toLowerCase() === 'admin' &&(
+              {user.role.toLowerCase() === "admin" && (
                 <Link href="/admin/dashboard">
                   <button className="w-full bg-[#213A58] text-white mt-4 p-2 hover:shadow-md hover:shadow-[#213A58cc] ">
                     Admin Dashboard
